d5ee328a00d0f976adfa2a8b7f1232d87dd2c82f
Ozgur Tuna Ozturk <oozturk18@uwcchina.org>
2021-01-20 09:31:08 +0300

    my final try

---
+++ precache-manifest.74df5e24252475bd57528c1dd840e6de.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "925c1c3481e345ae20f2492d7dd115cc",
+    "url": "/wwi-caves/index.html"
+  },
+  {
+    "revision": "76415e038172aab91d11",
+    "url": "/wwi-caves/static/css/main.444ab1b5.chunk.css"
+  },
+  {
+    "revision": "49b18a3ff616597ee5ba",
+    "url": "/wwi-caves/static/js/2.38c050c7.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/wwi-caves/static/js/2.38c050c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76415e038172aab91d11",
+    "url": "/wwi-caves/static/js/main.112887f0.chunk.js"
+  },
+  {
+    "revision": "f3675f4f2fac6209e993",
+    "url": "/wwi-caves/static/js/runtime-main.10c14317.js"
+  },
+  {
+    "revision": "e48fca8608fdd13caf7bbc023a96cbd9",
+    "url": "/wwi-caves/static/media/ampersand.e48fca86.woff2"
+  },
+  {
+    "revision": "43f0ba9e9f35c254f8fdffae2f43a144",
+    "url": "/wwi-caves/static/media/greenFlag.43f0ba9e.gif"
+  },
+  {
+    "revision": "1469e6fee6ca8bf5a08c3a518ea273af",
+    "url": "/wwi-caves/static/media/home.1469e6fe.gif"
+  }
+]);
